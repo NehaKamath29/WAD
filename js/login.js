e90492b3e610a9d5f7cefb38d39f8d041bd7864d
nishsatish-bmsce <nishantsatishkumar@gmail.com>
2022-11-30 23:44:52 +0530

    Validation+Profile

---
+++ js/login.js
@@ -0,0 +1,43 @@
+// Main Validation
+function check() {
+  password = document.getElementById('password').value;
+  username = document.getElementById('username').value;
+  clearErrors();
+
+  if (document.getElementById('username').value == '') {
+    seterror('name', '*username should not be blank');
+    returnval = false;
+    document.getElementById('username').focus();
+  } else {
+    returnval = true;
+  }
+
+  if (returnval == true) {
+    if (document.getElementById('password').value == '') {
+      seterror('password1', '*password should not be blank');
+      returnval = false;
+      document.getElementById('password').focus();
+    } else {
+      returnval = true;
+    }
+  }
+
+  if (returnval == true) {
+    window.location = 'hotels.html';
+  }
+}
+
+// Clear error messages
+function clearErrors() {
+  errors = document.getElementsByClassName('formerror');
+  for (let item of errors) {
+    item.innerHTML = '';
+  }
+}
+
+// Populate error containers
+function seterror(id, error) {
+  //sets error inside tag of id
+  element = document.getElementById(id);
+  element.getElementsByClassName('formerror')[0].innerHTML = error;
+}
